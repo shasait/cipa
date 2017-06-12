80b8cac5c737c6e98e1270be3d3a8380b86c56c2
Sebastian Hasait <sebastian@hasait.de>
2017-06-12 23:07:49 +0200

    Try to fix limitation of JENKINS-41339

--- src/de/hasait/cipa/Cipa.groovy
+++ src/de/hasait/cipa/Cipa.groovy
@@ -181,7 +181,7 @@ class Cipa implements Serializable {
 					def mvnRepo = determineMvnRepo()
 					script.echo("[CIPActivities] mvnRepo: ${mvnRepo}")
 					envVars.add("${ENV_VAR___MVN_REPO}=${mvnRepo}")
-					envVars.add("MAVEN_OPTS=-Dmaven.multiModuleProjectDirectory=\"${toolHome}\" ${toolMvn.options} ${script.env.MAVEN_OPTS}")
+					envVars.add("MAVEN_OPTS=-Dmaven.multiModuleProjectDirectory=\"${toolHome}\" ${toolMvn.options} ${script.env.MAVEN_OPTS ?: ''}")
 				}
 				for (configFileEnvVar in tool.configFileEnvVars) {
 					configFiles.add(script.configFile(fileId: configFileEnvVar.value, variable: configFileEnvVar.key))
@@ -266,7 +266,7 @@ class Cipa implements Serializable {
 
 		def optionsString = options.join(' ')
 
-		script.withEnv(["MAVEN_OPTS=${optionsString} ${script.env.MAVEN_OPTS}"]) {
+		script.withEnv(["MAVEN_OPTS=${optionsString} ${script.env.MAVEN_OPTS ?: ''}"]) {
 			script.sh(script: 'printenv | sort')
 			return script.sh(script: "mvn ${allArgumentsString}", returnStdout: returnStdout)
 		}
