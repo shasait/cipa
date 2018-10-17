67f969c2de94b048317d32bef6b8d1e3c0962c10
Sebastian Hasait <sebastian@hasait.de>
2018-10-17 22:03:25 +0200

    Add JSON steps; order members

--- src/de/hasait/cipa/PScript.groovy
+++ src/de/hasait/cipa/PScript.groovy
@@ -37,6 +37,10 @@ class PScript implements Serializable {
 		this.rawScript = rawScript
 	}
 
+	void archiveArtifacts(String artifacts, String excludes = null, boolean allowEmptyArchive = false, boolean fingerprint = false, boolean onlyIfSuccessful = false, boolean defaultExcludes = true, boolean caseSensitive = true) {
+		rawScript.archiveArtifacts(allowEmptyArchive: allowEmptyArchive, artifacts: artifacts, caseSensitive: caseSensitive, defaultExcludes: defaultExcludes, excludes: excludes, fingerprint: fingerprint, onlyIfSuccessful: onlyIfSuccessful)
+	}
+
 	RunWrapper build(String job, List parameters = [], int embedLogMaxLines = 100) {
 		RunWrapper runWrapper = rawScript.build(job: job, parameters: parameters, propagate: false)
 		Run<?, ?> rawBuild = runWrapper.rawBuild
@@ -46,169 +50,6 @@ class PScript implements Serializable {
 			throw new RuntimeException("Build ${rawBuild.url} failed!")
 		}
 		return RunWrapper
-	}
-
-	String determineHostname() {
-		String hostnameRaw = rawScript.sh(returnStdout: true, script: 'hostname')
-		return hostnameRaw.trim()
-	}
-
-	String determineMvnRepo() {
-		String workspace = rawScript.env.WORKSPACE
-		return workspace + '/.repo'
-	}
-
-	/**
-	 * Determine SVN URL of current working directory.
-	 */
-	String determineSvnUrlOfCwd() {
-		String svnUrl = sh('svn info | awk \'/^URL/{print $2}\'', true)
-		return svnUrl.trim()
-	}
-
-	/**
-	 * Determine SVN Revision of current working directory.
-	 */
-	String determineSvnRevOfCwd() {
-		String svnRev = sh('svn info | awk \'/^Revision/{print $2}\'', true)
-		return svnRev.trim()
-	}
-
-	/**
-	 * Determine Git Branch of current working directory.
-	 */
-	String determineGitBranchOfCwd() {
-		String gitRef = sh('git symbolic-ref HEAD', true).trim()
-		String prefix = 'refs/heads/'
-		if (gitRef.startsWith(prefix)) {
-			return gitRef.substring(prefix.length())
-		}
-		return gitRef
-	}
-
-	/**
-	 * Determine Git Revision of current working directory.
-	 */
-	String determineGitRevOfCwd() {
-		String gitRev = sh('git rev-parse HEAD', true)
-		return gitRev.trim()
-	}
-
-	void echo(String message) {
-		rawScript.echo '[Script] ' + message
-	}
-
-	void echoStacktrace(String message, Throwable throwable) {
-		echo(message + ': ' + extractStacktrace(throwable))
-	}
-
-	@NonCPS
-	private String extractStacktrace(Throwable throwable) {
-		StringWriter sw = new StringWriter()
-		PrintWriter pw = new PrintWriter(sw)
-		throwable.printStackTrace(pw)
-		pw.flush()
-		return sw.toString()
-	}
-
-	public <V> V dir(String dirname, Closure<V> body) {
-		rawScript.dir(dirname, body)
-	}
-
-	void deleteDir() {
-		rawScript.deleteDir()
-	}
-
-	String pwd() {
-		return rawScript.pwd()
-	}
-
-	String sh(String script, boolean returnStdout = false) {
-		return rawScript.sh(script: script, returnStdout: returnStdout)
-	}
-
-	String shAsUser(String username, String script, boolean returnStdout = false) {
-		String escapedScript = script.replace('\\', '\\\\').replace('"', '\\"')
-		return sh('echo "' + escapedScript + '" | ssh -T -o "BatchMode yes" ' + username + '@localhost', returnStdout)
-	}
-
-	boolean fileExists(final String relativePath) {
-		return rawScript.fileExists(relativePath)
-	}
-
-	public <V> V timeout(int timeoutInMinutes, Closure<V> body) {
-		rawScript.timeout(timeoutInMinutes, body)
-	}
-
-	public <V> V stage(String name, Closure<V> body) {
-		rawScript.stage(name, body)
-	}
-
-	void writeFile(String filepath, String content, String encoding = 'UTF-8') {
-		rawScript.writeFile(encoding: encoding, file: filepath, text: content)
-	}
-
-	String readFile(String filepath, String encoding = 'UTF-8') {
-		return rawScript.readFile(encoding: encoding, file: filepath)
-	}
-
-	void stash(String id, Set<String> includes = [], Set<String> excludes = [], boolean useDefaultExcludes = true, boolean allowEmpty = false) {
-		rawScript.stash(name: id, includes: includes.join(','), excludes: excludes.join(','), useDefaultExcludes: useDefaultExcludes, allowEmpty: allowEmpty)
-	}
-
-	void unstash(String id) {
-		rawScript.unstash(id)
-	}
-
-	void sleep(int seconds) {
-		rawScript.sleep(seconds)
-	}
-
-	void archiveArtifacts(String artifacts, String excludes = null, boolean allowEmptyArchive = false, boolean fingerprint = false, boolean onlyIfSuccessful = false, boolean defaultExcludes = true, boolean caseSensitive = true) {
-		rawScript.archiveArtifacts(allowEmptyArchive: allowEmptyArchive, artifacts: artifacts, caseSensitive: caseSensitive, defaultExcludes: defaultExcludes, excludes: excludes, fingerprint: fingerprint, onlyIfSuccessful: onlyIfSuccessful)
-	}
-
-	@NonCPS
-	int getCurrentBuildNumber() {
-		return rawScript.currentBuild.number
-	}
-
-	@NonCPS
-	Run<?, ?> getCurrentRawBuild() {
-		return rawScript.currentBuild.rawBuild
-	}
-
-	String mvn(
-			List<String> goals,
-			List<String> profiles = [],
-			List<String> arguments = [],
-			List<String> options = [],
-			boolean returnStdout = false,
-			List<String> mvnStdoutFilters = ['[INFO] Building', '[INFO] BUILD ', '[INFO] Finished at']
-	) {
-		def allArguments = ['-B', '-V', '-e']
-		if (rawScript.env[Cipa.ENV_VAR___MVN_SETTINGS]) {
-			allArguments.add('-s "${' + Cipa.ENV_VAR___MVN_SETTINGS + '}"')
-		}
-		if (rawScript.env[Cipa.ENV_VAR___MVN_TOOLCHAINS]) {
-			allArguments.add('--global-toolchains "${' + Cipa.ENV_VAR___MVN_TOOLCHAINS + '}"')
-		}
-		allArguments.add('-Dmaven.repo.local="${' + Cipa.ENV_VAR___MVN_REPO + '}"')
-		if (!profiles.empty) {
-			allArguments.add('-P' + profiles.join(','))
-		}
-		allArguments.addAll(goals)
-		allArguments.addAll(arguments)
-
-		def allArgumentsString = allArguments.empty ? '' : allArguments.join(' ')
-
-		def optionsString = options.join(' ')
-
-		rawScript.withEnv(["${Cipa.ENV_VAR___MVN_OPTIONS}=${optionsString} ${rawScript.env[Cipa.ENV_VAR___MVN_OPTIONS] ?: ''}"]) {
-			writeFile(MVN_LOG, "Executing: mvn ${allArgumentsString}\nEnvironment:\n")
-			sh("printenv | sort | tee -a ${MVN_LOG}")
-			return sh("set -o pipefail ; mvn ${allArgumentsString} | tee -a ${MVN_LOG}" + buildGrep(mvnStdoutFilters), returnStdout)
-		}
 	}
 
 	CheckoutResult checkout(CheckoutConfiguration config, String forcedScmBranch = null) {
@@ -361,21 +202,6 @@ class PScript implements Serializable {
 		return new CheckoutResult(scmUrl: scmUrl, scmRef: scmRef, scmResolvedBranch: scmResolvedBranch, scmRev: scmRev)
 	}
 
-	static class CheckoutResult {
-		String scmUrl
-		String scmRef
-		String scmResolvedBranch
-		String scmRev
-	}
-
-	@NonCPS
-	private String buildGrep(List<String> filters) {
-		if (filters) {
-			return '| grep -E "' + filters.collect { it.replace('[', '\\[').replace(']', '\\]').replace('"', '\\"') }.join('|') + '"'
-		}
-		return ''
-	}
-
 	@NonCPS
 	List<String> collectDescriptions(Job<?, ?> job = currentRawBuild.parent) {
 		List<String> descriptions = new ArrayList<>()
@@ -389,6 +215,203 @@ class PScript implements Serializable {
 			current = current.hasProperty('parent') ? current.parent : null
 		}
 		return descriptions
+	}
+
+	void deleteDir() {
+		rawScript.deleteDir()
+	}
+
+	String determineHostname() {
+		String hostnameRaw = rawScript.sh(returnStdout: true, script: 'hostname')
+		return hostnameRaw.trim()
+	}
+
+	String determineMvnRepo() {
+		String workspace = rawScript.env.WORKSPACE
+		return workspace + '/.repo'
+	}
+
+	/**
+	 * Determine SVN URL of current working directory.
+	 */
+	String determineSvnUrlOfCwd() {
+		String svnUrl = sh('svn info | awk \'/^URL/{print $2}\'', true)
+		return svnUrl.trim()
+	}
+
+	/**
+	 * Determine SVN Revision of current working directory.
+	 */
+	String determineSvnRevOfCwd() {
+		String svnRev = sh('svn info | awk \'/^Revision/{print $2}\'', true)
+		return svnRev.trim()
+	}
+
+	/**
+	 * Determine Git Branch of current working directory.
+	 */
+	String determineGitBranchOfCwd() {
+		String gitRef = sh('git symbolic-ref HEAD', true).trim()
+		String prefix = 'refs/heads/'
+		if (gitRef.startsWith(prefix)) {
+			return gitRef.substring(prefix.length())
+		}
+		return gitRef
+	}
+
+	/**
+	 * Determine Git Revision of current working directory.
+	 */
+	String determineGitRevOfCwd() {
+		String gitRev = sh('git rev-parse HEAD', true)
+		return gitRev.trim()
+	}
+
+	public <V> V dir(String dirname, Closure<V> body) {
+		rawScript.dir(dirname, body)
+	}
+
+	void echo(String message) {
+		rawScript.echo '[Script] ' + message
+	}
+
+	void echoStacktrace(String message, Throwable throwable) {
+		echo(message + ': ' + extractStacktrace(throwable))
+	}
+
+	@NonCPS
+	private String extractStacktrace(Throwable throwable) {
+		StringWriter sw = new StringWriter()
+		PrintWriter pw = new PrintWriter(sw)
+		throwable.printStackTrace(pw)
+		pw.flush()
+		return sw.toString()
+	}
+
+	boolean fileExists(String relativePath) {
+		return rawScript.fileExists(relativePath)
+	}
+
+	@NonCPS
+	int getCurrentBuildNumber() {
+		return rawScript.currentBuild.number
+	}
+
+	@NonCPS
+	Run<?, ?> getCurrentRawBuild() {
+		return rawScript.currentBuild.rawBuild
+	}
+
+	@NonCPS
+	def getItemByFullName(String fullName) {
+		return jenkins.getItemByFullName(fullName)
+	}
+
+	// not Jenkins return type to allow testing
+	@NonCPS
+	def getJenkins() {
+		def current = currentRawBuild
+		while (current.hasProperty('parent') && current.parent != null) {
+			current = current.parent
+		}
+		return current
+	}
+
+	String mvn(
+			List<String> goals,
+			List<String> profiles = [],
+			List<String> arguments = [],
+			List<String> options = [],
+			boolean returnStdout = false,
+			List<String> mvnStdoutFilters = ['[INFO] Building', '[INFO] BUILD ', '[INFO] Finished at']
+	) {
+		def allArguments = ['-B', '-V', '-e']
+		if (rawScript.env[Cipa.ENV_VAR___MVN_SETTINGS]) {
+			allArguments.add('-s "${' + Cipa.ENV_VAR___MVN_SETTINGS + '}"')
+		}
+		if (rawScript.env[Cipa.ENV_VAR___MVN_TOOLCHAINS]) {
+			allArguments.add('--global-toolchains "${' + Cipa.ENV_VAR___MVN_TOOLCHAINS + '}"')
+		}
+		allArguments.add('-Dmaven.repo.local="${' + Cipa.ENV_VAR___MVN_REPO + '}"')
+		if (!profiles.empty) {
+			allArguments.add('-P' + profiles.join(','))
+		}
+		allArguments.addAll(goals)
+		allArguments.addAll(arguments)
+
+		def allArgumentsString = allArguments.empty ? '' : allArguments.join(' ')
+
+		def optionsString = options.join(' ')
+
+		rawScript.withEnv(["${Cipa.ENV_VAR___MVN_OPTIONS}=${optionsString} ${rawScript.env[Cipa.ENV_VAR___MVN_OPTIONS] ?: ''}"]) {
+			writeFile(MVN_LOG, "Executing: mvn ${allArgumentsString}\nEnvironment:\n")
+			sh("printenv | sort | tee -a ${MVN_LOG}")
+			return sh("set -o pipefail ; mvn ${allArgumentsString} | tee -a ${MVN_LOG}" + buildGrep(mvnStdoutFilters), returnStdout)
+		}
+	}
+
+	String pwd() {
+		return rawScript.pwd()
+	}
+
+	String readFile(String filepath, String encoding = 'UTF-8') {
+		return rawScript.readFile(encoding: encoding, file: filepath)
+	}
+
+	def readJsonFile(String filepath) {
+		return rawScript.readJSON(file: filepath)
+	}
+
+	def readJsonText(String text) {
+		return rawScript.readJSON(text: text)
+	}
+
+	String sh(String script, boolean returnStdout = false) {
+		return rawScript.sh(script: script, returnStdout: returnStdout)
+	}
+
+	String shAsUser(String username, String script, boolean returnStdout = false) {
+		String escapedScript = script.replace('\\', '\\\\').replace('"', '\\"')
+		return sh('echo "' + escapedScript + '" | ssh -T -o "BatchMode yes" ' + username + '@localhost', returnStdout)
+	}
+
+	void sleep(int seconds) {
+		rawScript.sleep(seconds)
+	}
+
+	public <V> V stage(String name, Closure<V> body) {
+		rawScript.stage(name, body)
+	}
+
+	void stash(String id, Set<String> includes = [], Set<String> excludes = [], boolean useDefaultExcludes = true, boolean allowEmpty = false) {
+		rawScript.stash(name: id, includes: includes.join(','), excludes: excludes.join(','), useDefaultExcludes: useDefaultExcludes, allowEmpty: allowEmpty)
+	}
+
+	public <V> V timeout(int timeoutInMinutes, Closure<V> body) {
+		rawScript.timeout(timeoutInMinutes, body)
+	}
+
+	void writeFile(String filepath, String content, String encoding = 'UTF-8') {
+		rawScript.writeFile(encoding: encoding, file: filepath, text: content)
+	}
+
+	void unstash(String id) {
+		rawScript.unstash(id)
+	}
+
+	static class CheckoutResult {
+		String scmUrl
+		String scmRef
+		String scmResolvedBranch
+		String scmRev
+	}
+
+	@NonCPS
+	private String buildGrep(List<String> filters) {
+		if (filters) {
+			return '| grep -E "' + filters.collect { it.replace('[', '\\[').replace(']', '\\]').replace('"', '\\"') }.join('|') + '"'
+		}
+		return ''
 	}
 
 	@NonCPS
