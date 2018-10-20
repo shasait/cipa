20217c00744967eabdf6fbafd6e559e8190c8313
Sebastian Hasait <sebastian@hasait.de>
2018-10-20 14:57:42 +0200

    CipaActivityBuilder: Fix CPS.

--- src/de/hasait/cipa/internal/CipaActivityBuilder.groovy
+++ src/de/hasait/cipa/internal/CipaActivityBuilder.groovy
@@ -33,9 +33,9 @@ class CipaActivityBuilder implements Serializable {
 	private final Cipa cipa
 	private final CipaNode node
 
-	private final Set<CipaResourceWithState<?>> activityRequiresRead = []
-	private final Set<CipaResourceWithState<?>> activityRequiresWrite = []
-	private final Set<CipaResourceWithState<?>> activityProvides = []
+	private final Set<CipaResourceWithState<?>> requiresRead = []
+	private final Set<CipaResourceWithState<?>> requiresWrite = []
+	private final Set<CipaResourceWithState<?>> provides = []
 
 	private boolean used = false
 
@@ -52,79 +52,92 @@ class CipaActivityBuilder implements Serializable {
 	@NonCPS
 	CipaResourceWithState<CipaFileResource> providesDir(String relDir, boolean global = false) {
 		CipaResourceWithState<CipaFileResource> cipaResourceWithState = cipa.newFileResourceWithState(global ? null : node, relDir, newState())
-		activityProvides.add(cipaResourceWithState)
+		provides.add(cipaResourceWithState)
 		return cipaResourceWithState
 	}
 
 	@NonCPS
 	public <R extends CipaResource> CipaResourceWithState<R> provides(R resource) {
 		CipaResourceWithState<R> cipaResourceWithState = cipa.newResourceWithState(resource, newState())
-		activityProvides.add(cipaResourceWithState)
+		provides.add(cipaResourceWithState)
 		return cipaResourceWithState
 	}
 
 	@NonCPS
 	public <R extends CipaResource> CipaResourceWithState<R> modifies(CipaResourceWithState<R> modified) {
-		activityRequiresWrite.add(modified)
+		requiresWrite.add(modified)
 		CipaResourceWithState<R> newResourceState = cipa.newResourceState(modified, newState())
-		activityProvides.add(newResourceState)
+		provides.add(newResourceState)
 		return newResourceState
 	}
 
 	@NonCPS
 	public <R extends CipaResource> void reads(CipaResourceWithState<R> read) {
-		activityRequiresRead.add(read)
+		requiresRead.add(read)
 	}
 
 	@NonCPS
 	CipaActivity create(String name, Closure<?> logic) {
-		if (activityProvides && used) {
+		if (provides && used) {
 			throw new IllegalStateException("A builder having provided resources can be only used once: ${name}")
 		}
 
-		String activityName = name
-		CipaNode activityNode = node
-		Closure<?> activityLogic = logic
+		used = true
+		return new BuildActivity(cipa, node, name, logic, requiresRead, requiresWrite, provides);
+	}
 
-		CipaActivity activity = new CipaActivity() {
+	private static class BuildActivity implements CipaActivity, Serializable {
 
-			@NonCPS
-			String getName() {
-				return activityName
-			}
+		private final String name
+		private final CipaNode node
+		private final Closure<?> logic
+		private final Set<CipaResourceWithState<?>> requiresRead
+		private final Set<CipaResourceWithState<?>> requiresWrite
+		private final Set<CipaResourceWithState<?>> provides
 
-			@NonCPS
-			Set<CipaResourceWithState<?>> getRunRequiresRead() {
-				return activityRequiresRead
-			}
+		BuildActivity(Cipa cipa, CipaNode node, String name, Closure<?> logic,
+					  Set<CipaResourceWithState<?>> requiresRead, Set<CipaResourceWithState<?>> requiresWrite, Set<CipaResourceWithState<?>> provides) {
+			this.name = name
+			this.node = node
+			this.logic = logic
+			this.requiresRead = requiresRead
+			this.requiresWrite = requiresWrite
+			this.provides = provides
 
-			@NonCPS
-			Set<CipaResourceWithState<?>> getRunRequiresWrite() {
-				return activityRequiresWrite
-			}
-
-			@NonCPS
-			Set<CipaResourceWithState<?>> getRunProvides() {
-				return activityProvides
-			}
-
-			@NonCPS
-			CipaNode getNode() {
-				return activityNode
-			}
-
-			void prepareNode() {
-				// empty
-			}
-
-			void runActivity(CipaActivityRunContext runContext) {
-				activityLogic.call(runContext)
-			}
-
+			cipa.addBean(this)
 		}
 
-		used = true
-		return cipa.addBean(activity)
+		String getName() {
+			return name
+		}
+
+		CipaNode getNode() {
+			return node
+		}
+
+		@NonCPS
+		Set<CipaResourceWithState<?>> getRunRequiresRead() {
+			return requiresRead
+		}
+
+		@NonCPS
+		Set<CipaResourceWithState<?>> getRunRequiresWrite() {
+			return requiresWrite
+		}
+
+		@NonCPS
+		Set<CipaResourceWithState<?>> getRunProvides() {
+			return provides
+		}
+
+		void prepareNode() {
+			// nop
+		}
+
+		void runActivity(CipaActivityRunContext runContext) {
+			logic.call(runContext)
+		}
+
 	}
 
 }
