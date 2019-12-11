bd3eff2350db5b0d4c7711329cce832d6bffe2f1
Azam Afzaal <external.azam.tahir@kuehne-nagel.com>
2019-12-11 14:13:08 +0100

    [SALOGINFRA-7136] refactored CipaCleanupNodeActivity class

--- src/de/hasait/cipa/activity/CipaCleanupNodeActivity.groovy
+++ src/de/hasait/cipa/activity/CipaCleanupNodeActivity.groovy
@@ -2,47 +2,22 @@ package de.hasait.cipa.activity
 
 import com.cloudbees.groovy.cps.NonCPS
 import de.hasait.cipa.Cipa
-import de.hasait.cipa.internal.CipaActivityWrapper
-import de.hasait.cipa.resource.CipaFileResource
 
 /**
- * Activity which cleans up all the resources.
+ * Activity which cleans up all the resources from jenkins
+ * workspace for every node.
  */
-class CipaCleanupNodeActivity extends AbstractCipaAroundActivity implements CipaAfterActivities, Serializable {
-
-    public static final int AROUND_ACTIVITY_ORDER = 10000
-    private Set<CipaFileResource> fileResources
-    private String resourcePath
-    private String pwd
-
+class CipaCleanupNodeActivity extends AbstractCipaBean implements CipaAfterActivities, Serializable {
 
     CipaCleanupNodeActivity(Cipa cipa) {
         super(cipa)
-        fileResources = new HashSet<>()
-    }
-
-    @NonCPS
-    @Override
-    void runAroundActivity(CipaActivityWrapper wrapper, Closure<?> next) {
-        wrapper.getActivity().getRunProvides().each {
-            def resource = it.getResource()
-            if (resource instanceof CipaFileResource) {
-                fileResources.add(resource)
-            }
-        }
-        super.runAroundActivity(wrapper, next)
     }
 
     @NonCPS
     @Override
     void afterCipaActivities() {
-        rawScript.echo("ws: ${rawScript.env.WORKSPACE}")
-        rawScript.echo("pwd: ${rawScript.pwd()}")
-    }
-
-    @NonCPS
-    @Override
-    int getRunAroundActivityOrder() {
-        return AROUND_ACTIVITY_ORDER
+        rawScript.echo("Jenkins workspace: ${rawScript.env.WORKSPACE}")
+        rawScript.echo("Cleaning up ${rawScript.pwd()} base directory...")
+        rawScript.deleteDir()
     }
 }
