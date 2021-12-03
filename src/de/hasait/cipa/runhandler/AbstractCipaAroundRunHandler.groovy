fc8a3537e8a751602abcaa20bd3b9c598c5a8d1a
Sebastian Hasait <sebastian@hasait.de>
2021-12-03 08:41:17 +0100

    Add AbstractCipaAroundRunHandler - a CipaRunHandler with beforeRun and afterRun methods

---
+++ src/de/hasait/cipa/runhandler/AbstractCipaAroundRunHandler.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 by Sebastian Hasait (sebastian at hasait dot de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.hasait.cipa.runhandler
+
+abstract class AbstractCipaAroundRunHandler extends AbstractCipaRunHandler {
+
+	AbstractCipaAroundRunHandler(Object rawScriptOrCipa) {
+		super(rawScriptOrCipa)
+	}
+
+	@Override
+	final void handleRun(Closure<?> next) {
+		beforeRun()
+		try {
+			next.call()
+		} catch (Exception e) {
+			afterRun(e)
+			throw e
+		}
+		afterRun(null)
+	}
+
+	void beforeRun() {
+		// empty default implementation
+	}
+
+	void afterRun(Exception e) {
+		// empty default implementation
+	}
+
+}
